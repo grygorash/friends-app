bdc776cdcf3486f3d59be82dc381388a18cc8643
grygorash <vyacheslav.grygorash@gmail.com>
2018-05-18 12:55:22 +0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import registerServiceWorker from "./registerServiceWorker";
